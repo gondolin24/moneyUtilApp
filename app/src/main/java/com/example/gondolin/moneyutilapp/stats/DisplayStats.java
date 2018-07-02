d6515db60628c858ee6a61d4388898802b84dea2
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2018-07-02 17:06:27 -0400

    Account stats class

---
+++ app/src/main/java/com/example/gondolin/moneyutilapp/stats/DisplayStats.java
@@ -0,0 +1,16 @@
+package com.example.gondolin.moneyutilapp.stats;
+
+
+import com.example.gondolin.moneyutilapp.models.Account;
+
+public class DisplayStats {
+
+
+
+    public double getDailyAmount(Account account) {
+
+        return account.getBalance();
+
+    }
+
+}
