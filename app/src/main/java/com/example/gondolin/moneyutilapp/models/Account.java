80fa55483ed28a5b910e4a7d9714fd13d39cdeae
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2018-07-02 17:05:53 -0400

    Added Account class

---
+++ app/src/main/java/com/example/gondolin/moneyutilapp/models/Account.java
@@ -0,0 +1,15 @@
+package com.example.gondolin.moneyutilapp.models;
+
+
+public abstract class Account {
+
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
