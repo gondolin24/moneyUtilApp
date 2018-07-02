96dbdd2d5a6e37998f0f207ded185f5dac9259ff
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2018-07-02 17:06:19 -0400

    account interfwace

---
+++ app/src/main/java/com/example/gondolin/moneyutilapp/models/SimpleBankActions.java
@@ -0,0 +1,10 @@
+package com.example.gondolin.moneyutilapp.models;
+
+
+public interface SimpleBankActions {
+
+    void decreaseBalance(double amount);
+
+    void increaseBalance(double amount);
+
+}
